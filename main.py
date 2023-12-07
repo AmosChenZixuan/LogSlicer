7b826f8c9ac60127a18aebbb5a981a969aa46516
Zixuan Chen (Amos) <zchen152@volvocars.com>
2023-12-07 15:31:21 +0100

    use tempfile to store downloaded content

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import JSONResponse
 from urllib.parse import urlparse
 from pymongo import MongoClient
 import os
 import uuid
 import requests
+import tempfile
 
 from pipeline import run_pipeline
 
@@ -24,12 +26,12 @@ def create_job(url: str, id: str):
         # download file
         jobs.insert_one({'_id': id, 'status': 'downloading', 'result': ''})
         #r = requests.get(url, allow_redirects=True)
-        filename = get_filename_from_url(url)
-        filedir = os.path.join('tmp', id)
-        os.makedirs(filedir, exist_ok=True)
-        filepath = os.path.join(filedir, filename)
-        #open(filepath, 'wb').write(r.content)
-        open(filepath, 'w').write('testing') ## TODO
+        os.makedirs('tmp', exist_ok=True)
+        with tempfile.NamedTemporaryFile(dir='tmp', mode='wb', delete=False) as tmp:
+            #tmp.write(r.content)
+            tmp.write(open(url, 'rb').read())
+            filepath = tmp.name
+
         # execute job
         jobs.update_one({'_id': id}, {'$set': {'status': 'executing'}})
         res = run_pipeline(filepath)
@@ -44,7 +46,9 @@ def create_job(url: str, id: str):
 async def run_summarization_chain(url: str, background_tasks: BackgroundTasks):
     id = str(uuid.uuid4())
     background_tasks.add_task(create_job, url, id)
-    return {"message": "Job has been submitted", "id": id}
+    return JSONResponse(status_code=201,
+                        content = {"message": "Job has been submitted", 
+                                   "id": id})
 
 @app.get("/status/{id}")
 async def read_job_status(id):
@@ -52,7 +56,8 @@ async def read_job_status(id):
     if job is not None:
         return {"status": job['status']}
     else:
-        return {"status": "not found"}
+        return JSONResponse(status_code=404,
+                            content = {"message": "Job id not found"})
 
 @app.get("/result/{id}")
 async def read_job_result(id):
@@ -60,7 +65,8 @@ async def read_job_result(id):
     if job is not None:
         return {"result": job['result']}
     else:
-        return {"result": "not found"}
+        return JSONResponse(status_code=404,
+                            content = {"message": "Job id not found"})
 
 @app.get("/")
 async def home():
