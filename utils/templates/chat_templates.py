32e922f23b4c4896211d97306fb555a65e0dd45f
Zixuan Chen (Amos) <zchen152@volvocars.com>
2023-12-15 16:32:01 +0100

    update prompt to reinforce format

--- utils/templates/chat_templates.py
+++ utils/templates/chat_templates.py
@@ -14,9 +14,8 @@ class SystemPromptMappingTemplate(BaseStrTemplate):
         1. Read and understand the relation among the occurrence count, log type and the payload.
         2. Point out the important errors in the log.
         3. Identify potential root causes of the errors.
-        4. Provide suggestions if there are any. 
-        5. Organize information and write a concise summary.
-        6. Revise to remove any redundant, inaacurate or vague information.
+        4. Organize information and write a concise summary.
+        5. Revise to remove any redundant or vague information.
         Summary:
         '''
         super().__init__(_template, input_variables = ['text'], **kargs)
@@ -31,10 +30,26 @@ class SystemPromptConbineTemplate(BaseStrTemplate):
         Draft the summary following these steps:
         1. Identify common points among the summaries. These will be the backbone of the final summary.
         2. Point out unique information in each summary. These could be important details.
-        3. Organize information and write a concise summary. It should have one section for summary and one section for suggestions.
-        4. Revise to remove any redundant, inaacurate or vague information.
-        5. Write the final summary in formatted Markdown, ensuring the text width does not exceed 100 characters. Use bullet points for content organization.
-        Make sure it has one section for summary and one section for suggestions.
+        3. Organize information and write a concise summary.
+        4. Revise to remove any redundant or vague information.
+        5. Provide suggestions to resolve the errors. Be specific and actionable.
+        5. Write the final summary in formatted Markdown. 
+        Requirements on the format:
+        - The final summary should be written in Markdown.
+        - The final summary should has one section for summary and one section for suggestions.
+        - Use bullet points for content organization.
+        - When mentioning multiple session in one bullet point, create a sub-bullet list for each session instead.
+        Example:
+        {{
+        ## Summary
+        - Problem 1
+            - Session 1
+            - Session 2
+        - Problem 2
+        ## Suggestions
+        - Suggestion 1
+        - Suggestion 2
+        }}
         Final Summary:
         '''
         super().__init__(_template, input_variables = ['text'], **kargs)
